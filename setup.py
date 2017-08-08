d0fb9e5ed845dd370532fb710759b35394a7cd16
Jack <jack.friedson@gmail.com>
2017-08-07 20:48:15 -0400

    Add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+def get_requirements_from_file(filepath):
+    requires = []
+    with open(filepath, 'r') as f:
+        requires.append(f.readline())
+    return requires
+
+setup(
+    name='cryptotrading',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=get_requirements_from_file('requirements.txt')
+)
