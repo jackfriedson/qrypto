0d5c521f0f30a4ec06dfcad97122a43e464110db
Jack <jack.friedson@gmail.com>
2017-10-28 16:44:31 -0400

    Abstract classifier strategy

--- qrypto/models/rnn_classifier.py
+++ qrypto/models/rnn_classifier.py
@@ -9,20 +9,25 @@ class RNNClassifier(object):
     def __init__(self,
                  scope: str,
                  n_inputs: int,
-                 n_outputs: int,
                  hidden_units: int = None,
                  learn_rate: float = 0.0005,
                  renorm_decay: float = 0.99,
-                 dropout_keep_prob: float = 1.0,
                  rnn_layers: int = 1,
+                 dropout_prob: float = 0.,
+                 rnn_dropout_prob: float = 0.,
                  summaries_dir: str = None):
         self.scope = scope
+        self.n_inputs = n_inputs
+        self.rnn_layers = rnn_layers
+        n_hiddens = hidden_units or n_inputs
 
         with tf.variable_scope(scope):
             self.inputs = tf.placeholder(shape=[None, n_inputs], dtype=tf.float32, name='inputs')
-            self.labels = tf.placeholder(shape=[None], dtype=tf.int32, name='labels')
+            self.labels = tf.placeholder(shape=[None, 1], dtype=tf.int32, name='labels')
             self.phase = tf.placeholder(dtype=tf.bool, name='phase')
             self.trace_length = tf.placeholder(dtype=tf.int32, name='trace_length')
+
+            self.direction_labels = self.labels[:,0]
 
             batch_size = tf.reshape(tf.shape(self.inputs)[0] // self.trace_length, shape=[])
 
@@ -30,20 +35,18 @@ class RNNClassifier(object):
             self.norm_flat = tf.reshape(self.norm_layer, shape=[batch_size, self.trace_length, n_inputs])
 
             rnn_cell = tf.contrib.rnn.LSTMCell(num_units=n_inputs, state_is_tuple=True, activation=tf.nn.softsign, use_peepholes=True)
-            # TODO: try adding attention to the LSTM
-            # rnn_cell = tf.contrib.rnn.DropoutWrapper(rnn_cell, output_keep_prob=dropout_keep_prob)
+            rnn_cell = tf.contrib.rnn.DropoutWrapper(rnn_cell, output_keep_prob=1-rnn_dropout_prob)
             rnn_cell = tf.contrib.rnn.MultiRNNCell([rnn_cell] * rnn_layers, state_is_tuple=True)
 
             self.rnn_in = rnn_cell.zero_state(batch_size, dtype=tf.float32)
             self.rnn, self.rnn_state = tf.nn.dynamic_rnn(rnn_cell, self.norm_flat, dtype=tf.float32, initial_state=self.rnn_in)
             self.rnn = tf.reshape(self.rnn, shape=tf.shape(self.norm_layer))
 
-            n_hiddens = hidden_units or (n_inputs + n_outputs) // 2
             self.hidden_layer = tf.contrib.layers.fully_connected(self.rnn, n_hiddens, activation_fn=tf.nn.tanh)
-            self.output_layer = tf.contrib.layers.fully_connected(self.hidden_layer, n_outputs, activation_fn=None)
+            self.output_layer = tf.contrib.layers.fully_connected(self.hidden_layer, 2, activation_fn=None)
             self.probabilities = tf.nn.softmax(self.output_layer)
 
-            self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.output_layer, labels=self.labels)
+            self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.direction_labels, logits=self.output_layer)
             self.loss = tf.reduce_mean(self.losses)
             self.optimizer = tf.train.AdamOptimizer(learn_rate)
 
@@ -72,7 +75,8 @@ class RNNClassifier(object):
             self.trace_length: trace_length,
             self.rnn_in: rnn_state
         }
-        return sess.run([self.output_layer, self.probabilities, self.rnn_state], feed_dict)
+        out, prob, rnn = sess.run([self.output_layer, self.probabilities, self.rnn_state], feed_dict)
+        return (out, prob), rnn
 
     def update(self, sess, state, labels, trace_length, rnn_state):
         feed_dict = {
@@ -89,7 +93,7 @@ class RNNClassifier(object):
         if self.summary_writer:
             self.summary_writer.add_summary(summaries, global_step)
 
-        return loss
+        return (loss,)
 
     def compute_loss(self, sess, state, label, rnn_state):
         feed_dict = {
@@ -99,4 +103,8 @@ class RNNClassifier(object):
             self.trace_length: 1,
             self.rnn_in: rnn_state
         }
-        return sess.run(self.loss, feed_dict)
+        loss = sess.run(self.loss, feed_dict)
+        return (loss,)
+
+    def initial_rnn_state(self, size: int = 1):
+        return [(np.zeros([size, self.n_inputs]), np.zeros([size, self.n_inputs]))] * self.rnn_layers
