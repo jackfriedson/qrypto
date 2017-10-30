c679b25fefd44424b272855b061fafa35c99be07
Jack <jack.friedson@gmail.com>
2017-10-30 15:13:30 -0400

    Use full hidden layers

--- qrypto/models/rnn_multitask.py
+++ qrypto/models/rnn_multitask.py
@@ -58,24 +58,26 @@ class RNNMultiTaskLearner(object):
             hidden_layer = tf.contrib.layers.fully_connected(rnn, self.n_hiddens, activation_fn=tf.nn.tanh)
 
             # Task 1: Estimate Volatility
-            volatility_hidden = tf.contrib.layers.fully_connected(hidden_layer, self.n_hiddens // 2, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
+            volatility_hidden = tf.contrib.layers.fully_connected(hidden_layer, self.n_hiddens, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
             volatility_dropout = tf.layers.dropout(volatility_hidden, dropout_prob, training=self.phase)
             self.volatility_out = tf.contrib.layers.fully_connected(volatility_dropout, 1, activation_fn=None, weights_regularizer=l1_reg)
             self.volatility_out = tf.reshape(self.volatility_out, shape=[tf.shape(self.inputs)[0]])
             self.volatility_loss = tf.losses.absolute_difference(volatility_labels, self.volatility_out)
 
-            hidden_2 = tf.contrib.layers.fully_connected(hidden_layer, self.n_hiddens // 2, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
+            hidden_2 = tf.contrib.layers.fully_connected(hidden_layer, self.n_hiddens, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
 
             # Task 2: Classify Direction
-            direction_hidden = tf.contrib.layers.fully_connected(hidden_2, self.n_hiddens // 2, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
+            direction_hidden = tf.contrib.layers.fully_connected(hidden_2, self.n_hiddens, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
             direction_dropout = tf.layers.dropout(direction_hidden, dropout_prob, training=self.phase)
             self.direction_out = tf.contrib.layers.fully_connected(direction_dropout, 2, activation_fn=None, weights_regularizer=l1_reg)
             self.direction_out = tf.reshape(self.direction_out, shape=[tf.shape(self.inputs)[0], 2])
             direction_losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=direction_labels, logits=self.direction_out)
             self.direction_loss = tf.reduce_mean(direction_losses)
 
+            # TODO: add aleatoric uncertainty task
+
             # Task 3: Estimate Return
-            return_hidden = tf.contrib.layers.fully_connected(hidden_2, self.n_hiddens // 2, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
+            return_hidden = tf.contrib.layers.fully_connected(hidden_2, self.n_hiddens, activation_fn=tf.nn.tanh, weights_regularizer=l1_reg)
             return_dropout = tf.layers.dropout(return_hidden, dropout_prob, training=self.phase)
             self.return_out = tf.contrib.layers.fully_connected(return_dropout, 1, activation_fn=None, weights_regularizer=l1_reg)
             self.return_out = tf.reshape(self.return_out, shape=[tf.shape(self.inputs)[0]])
