3f86c2bb540f8b7e8718c540bedd576d798a5090
Jack <jack.friedson@gmail.com>
2017-10-31 20:45:55 -0400

    use return to determine direction

--- qrypto/strategy/qlearn/multitask.py
+++ qrypto/strategy/qlearn/multitask.py
@@ -18,4 +18,4 @@ class MultitaskStrategy(LearnStrategy):
 
     @staticmethod
     def _order_strategy(output, is_label: bool = False):
-        return output[0] if is_label else np.argmax(output[0][0])
+        return 1 if output[1] > 0 else 0
